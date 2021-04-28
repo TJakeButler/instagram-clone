903fba848d2b2db1803ab81a3d0eae8c62f6633a
Jake Butler <tjakebutler@gmail.com>
2021-04-28 12:56:28 -0500

    Styling posts, installed material UI, imported Avatar into Post component

--- src/Post.js
+++ src/Post.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
-    return (
-        <div>
-            <h3>Username</h3>
-            {/* header => + avatar + username */}
-            {/* image */}
-            {/* username + caption */}
+  return (
+    <div className="post">
+      <div className="post__header">
+      <Avatar
+        className="post__avatar"
+        alt="Jake"
+        src="/static/images/avatar/1.jpg"
+      />
 
-        </div>
-    )
+      <h3>Username</h3>
+      </div>
+      
+      <img
+        className="post__image"
+        src="https://www.freecodecamp.org/news/content/images/size/w600/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+      ></img>
+
+      <h4 className="post__text">
+        <strong>Jake</strong> working on my posts
+      </h4>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
