8b9fa0b124dd49d9ee1c153ad8444163e020cbec
Jake Butler <tjakebutler@gmail.com>
2021-04-28 12:24:22 -0500

    Created Header Component and Post Component

---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            <h3>Username</h3>
+            {/* header => + avatar + username */}
+            {/* image */}
+            {/* username + caption */}
+
+        </div>
+    )
+}
+
+export default Post
