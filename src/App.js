98c93c0133fd9c4464a4b76f24069796c8da9bf2
Jake Butler <tjakebutler@gmail.com>
2021-04-29 10:55:12 -0500

    Creating User Authentication using firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from "./firebase";
-import {makeStyles} from '@material-ui/core/styles'
-import Modal from '@material-ui/core/Modal'
-
+import { db, auth } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button } from "@material-ui/core";
+import { Input } from "@material-ui/core";
 
 function getModalStyle() {
   const top = 50;
@@ -19,58 +20,127 @@ function getModalStyle() {
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
 }));
 
-
-
 function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null)
+
+  // The useEfffect is a front end listener
+  
+  useEffect(() => {
+    // the auth.onAuthStateChanged is a back end listener
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in..
+        console.log("This is auth user >>>", authUser)
+        setUser(authUser)
+        
+      } else {
+      // user has logged out
+      setUser(null)
+      }
+    })
+
+    return () => {
+      // perform some cleanup actions
+      unsubscribe();
+    }    
+
+  }, [user, username])
 
   // USEEFFECT Runs a piece of code base on a specific condition
   useEffect(() => {
     // this is where the code runs
     db.collection("posts").onSnapshot((snapshot) => {
       // every time a new post is added, this code fires
-      setPosts(snapshot.docs.map(doc => ({
-       id: doc.id, 
-       post: doc.data()
-      })))
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
     });
   }, []);
+
   const handleClose = () => {
     setOpen(false);
   };
+
+  const signUp = (event) => {
+    event.preventDefault()
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((error) => alert(error.message))
+  };
+    
+
   return (
     <div className="app">
-      <Modal open={open} onClose={() => setOpen(false)}>
+      <Modal 
+      open={open} onClose={() => setOpen(false)}
+      >
         <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"></img>
+            </center>
+            <Input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>Sign Up</Button>
+          </form>
         </div>
       </Modal>
 
       <div className="app__header">
-
-        <img
-          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-          alt="Instagram Logo"
-          className="app_headerImage"
-        />
+        <center>
+          <img
+            className="app_headerImage"
+            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+            alt=""
+          />
+        </center>
       </div>
 
-      <Button></Button>
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
       <h1>Hello, welcome to Instagram Clone Built with React</h1>
 
-      {posts.map(({id, post}) => (
+      {posts.map(({ id, post }) => (
         <Post
           key={id}
           username={post.username}
