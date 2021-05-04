4516ab8030692beac44d8b589199bcf83ec3b048
Jake Butler <tjakebutler@gmail.com>
2021-05-04 14:58:58 -0500

    Added useEffect that listens for comments and updates the state of posts accordingly, also styling posts

--- src/Post.js
+++ src/Post.js
@@ -1,14 +1,35 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
+import { db } from "./firebase";
 
 function Post({postId, username, caption, imageUrl }) {
   
 const [comments, setComments] = useState([])
+const [comment, setComment] = useState('')
 
 
+// This useEffect is listening for anytime comments change in the db and when they do the state of comments is updated 
+useEffect(() => {
+  let unsubscribe;
+  if (postId) {
+    unsubscribe = db
+      .collection("posts")
+      .doc(postId)
+      .collection("comments")
+      .onSnapshot((snapshot) => {
+        setComments(snapshot.docs.map((doc) => doc.data()));
+      });
+  }
 
+  return () => {
+    unsubscribe();
+  };
+}, [postId]);
 
+const postComment = (event) => {
+
+}
 
   return (
     <div className="post">
@@ -27,8 +48,29 @@ const [comments, setComments] = useState([])
       <h4 className="post__text">
         <strong>{username}</strong> {caption}
       </h4>
+
+
+      <form className="post__commentBox">
+
+        <input
+        className="post__input"
+        type="text"
+        placeholder="Add a comment..."
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        >
+        </input>
+
+        <button
+        className="post__button"
+        disabled={!comment}
+        type="submit"
+        onClick={postComment}
+        >Post</button>
+      </form>
     </div>
   );
 }
 
 export default Post;
+  
