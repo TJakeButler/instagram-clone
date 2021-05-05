a86fdb7cf5cd0f22ae83f585bda702b1da010b71
Jake Butler <tjakebutler@gmail.com>
2021-05-05 13:21:12 -0500

    deployed to firebase

--- src/Post.js
+++ src/Post.js
@@ -1,45 +1,42 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
 import { db } from "./firebase";
-import firebase from 'firebase'
+import firebase from "firebase";
 
-function Post({postId, user, username, caption, imageUrl }) {
-  
-const [comments, setComments] = useState([])
-const [comment, setComment] = useState('')
+function Post({ postId, user, username, caption, imageUrl }) {
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
 
+  // This useEffect is listening for anytime comments change in the db and when they do the state of comments is updated
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection("posts")
+        .doc(postId)
+        .collection("comments")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
 
-// This useEffect is listening for anytime comments change in the db and when they do the state of comments is updated 
-useEffect(() => {
-  let unsubscribe;
-  if (postId) {
-    unsubscribe = db
-      .collection("posts")
-      .doc(postId)
-      .collection("comments")
-      .orderBy('timestamp', 'desc')
-      .onSnapshot((snapshot) => {
-        setComments(snapshot.docs.map((doc) => doc.data()));
-      });
-  }
+    return () => {
+      unsubscribe();
+    };
+  }, [postId]);
 
-  return () => {
-    unsubscribe();
+  const postComment = (event) => {
+    event.preventDefault();
+
+    db.collection("posts").doc(postId).collection("comments").add({
+      text: comment,
+      username: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setComment("");
   };
-}, [postId]);
-
-const postComment = (event) => {
-  event.preventDefault();
-
-
-  db.collection('posts').doc(postId).collection('comments').add({
-    text: comment,
-    username: user.displayName,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp()
-  })
-  setComment("")
-}
 
   return (
     <div className="post">
@@ -59,38 +56,36 @@ const postComment = (event) => {
         <strong>{username}</strong> {caption}
       </h4>
 
-        <div className="post__comments">
-          {comments.map((comment) => (
-            <p>
-              <strong>{comment.username}</strong> {comment.text}
-            </p>
-          ))}
-        </div>
+      <div className="post__comments">
+        {comments.map((comment) => (
+          <p>
+            <strong>{comment.username}</strong> {comment.text}
+          </p>
+        ))}
+      </div>
 
       {user && (
         <form className="post__commentBox">
-        <input
-        className="post__input"
-        type="text"
-        placeholder="Add a comment..."
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
-        >
-        </input>
+          <input
+            className="post__input"
+            type="text"
+            placeholder="Add a comment..."
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          ></input>
 
-        <button
-        className="post__button"
-        disabled={!comment}
-        type="submit"
-        onClick={postComment}
-        >Post</button>
-      </form>
+          <button
+            className="post__button"
+            disabled={!comment}
+            type="submit"
+            onClick={postComment}
+          >
+            Post
+          </button>
+        </form>
       )}
-
-      
     </div>
   );
 }
 
 export default Post;
-  
